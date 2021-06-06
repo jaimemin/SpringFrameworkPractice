389fdd6e247838802db0c8f7f2eaf45bdd41f8bf
jaimemin <jaimemin@gmail.com>
2021-06-07 02:27:10 +0900

    implemented MVC Pattern similar to Spring MVC Pattern

---
+++ src/main/java/com/tistory/jaimemin/servlet/web/frontcontroller/v4/controller/MemberSaveControllerV4.java
@@ -0,0 +1,28 @@
+package com.tistory.jaimemin.servlet.web.frontcontroller.v4.controller;
+
+import com.tistory.jaimemin.servlet.domain.member.Member;
+import com.tistory.jaimemin.servlet.domain.member.repository.MemberRepository;
+import com.tistory.jaimemin.servlet.web.frontcontroller.v4.ControllerV4;
+
+import java.util.Map;
+
+public class MemberSaveControllerV4 implements ControllerV4 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public String process(Map<String, String> paramMap, Map<String, Object> model) {
+        String username = paramMap.get("username");
+        int age = Integer.parseInt(paramMap.get("age"));
+
+        Member member = Member.builder()
+                .username(username)
+                .age(age)
+                .build();
+        memberRepository.save(member);
+
+        model.put("member", member);
+
+        return "save-result";
+    }
+}
---
+++ src/main/java/com/tistory/jaimemin/servlet/web/frontcontroller/v5/adapter/ControllerV4HandlerAdapter.java
@@ -0,0 +1,46 @@
+package com.tistory.jaimemin.servlet.web.frontcontroller.v5.adapter;
+
+import com.tistory.jaimemin.servlet.web.frontcontroller.ModelView;
+import com.tistory.jaimemin.servlet.web.frontcontroller.v4.ControllerV4;
+import com.tistory.jaimemin.servlet.web.frontcontroller.v5.MyHandlerAdapter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ControllerV4HandlerAdapter implements MyHandlerAdapter {
+
+    @Override
+    public boolean supports(Object handler) {
+        return (handler instanceof ControllerV4);
+    }
+
+    @Override
+    public ModelView handle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws ServletException, IOException {
+        ControllerV4 controllerV4 = (ControllerV4) handler;
+        Map<String, String> paramMap = getParamMap(request);
+        Map<String, Object> model = new HashMap<>();
+
+        String viewName = controllerV4.process(paramMap, model);
+        ModelView modelView = new ModelView(viewName);
+        modelView.setModel(model);
+
+        return modelView;
+    }
+
+    private Map<String, String> getParamMap(HttpServletRequest request) {
+        Map<String, String> paramMap = new HashMap<>();
+
+        request.getParameterNames()
+                .asIterator()
+                .forEachRemaining(paramName ->
+                        paramMap.put(paramName, request.getParameter(paramName))
+                );
+
+        return paramMap;
+    }
+}
