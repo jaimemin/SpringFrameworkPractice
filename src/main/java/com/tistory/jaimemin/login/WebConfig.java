7c678063e3c52669168e5b81aae6149c869346e7
jaimemin <jaimemin@gmail.com>
2021-08-01 02:19:48 +0900

    applied logFilter and loginCheckFilter

---
+++ src/main/java/com/tistory/jaimemin/login/WebConfig.java
@@ -0,0 +1,33 @@
+package com.tistory.jaimemin.login;
+
+import com.tistory.jaimemin.login.web.filter.LogFilter;
+import com.tistory.jaimemin.login.web.filter.LoginCheckFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.Filter;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public FilterRegistrationBean logFilter() {
+        FilterRegistrationBean<Filter> filterRegistration = new FilterRegistrationBean<>();
+        filterRegistration.setFilter(new LogFilter());
+        filterRegistration.setOrder(1);
+        filterRegistration.addUrlPatterns("/*");
+
+        return filterRegistration;
+    }
+
+    @Bean
+    public FilterRegistrationBean loginCheckFilter() {
+        FilterRegistrationBean<Filter> filterRegistration = new FilterRegistrationBean<>();
+        filterRegistration.setFilter(new LoginCheckFilter());
+        filterRegistration.setOrder(2);
+        filterRegistration.addUrlPatterns("/*");
+
+        return filterRegistration;
+    }
+}
